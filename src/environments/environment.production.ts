762a492f76aff16ea70d4212342c2d4485ae3c35
christopheB21 <christophe.brunel21@orange.fr>
2024-11-19 16:22:31 +0100

    Environment settings (#2)
    * Environment settings
    
    * Config environments

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyDzXEnhXWoTAVQzeYyJyh2JHhAlVeCD3Bg",
+        authDomain: "productivity-planner-prod01.firebaseapp.com",
+        projectId: "productivity-planner-prod01",
+        storageBucket: "productivity-planner-prod01.firebasestorage.app",
+        messagingSenderId: "561843573790",
+        appId: "1:561843573790:web:1c58a741a0e8d6c62e8a0f"
+      }
+};
