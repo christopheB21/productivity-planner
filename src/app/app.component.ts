1379fe23e14b85641e1a8a6c5b2dc70e7fd52320
christopheB21 <christophe.brunel21@orange.fr>
2024-11-22 10:58:40 +0100

    build(config): firebase aliases configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { environment } from "../environments/environment"
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'productivity-planner';
+  isProduction = environment.production;
+  name = environment.name;
 }
