368b6ed34b8f37028199238c1c4a16209ba7342e
christopheB21 <christophe.brunel21@orange.fr>
2024-12-07 19:02:31 +0100

    build(webapp): add firestore database save document

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthenticationService } from './core/authentication.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
+  readonly #authenticationService = inject(AuthenticationService);
+
+
   title = 'productivity-planner';
+  onLogin() {
+    const email = 'john.doe@gmail.com';
+    const password = 'azerty';
+    this.#authenticationService
+      .firebaseLogin(email,password)
+      .pipe(
+        switchMap(response =>  { 
+          console.log(response);
+          const { email, localId, idToken } = response;
+          return this.#authenticationService.writeFirebaseDatabase(email, localId, idToken);
+        })
+      )
+      .subscribe(response=> console.log(response));
+  }
 }
--- src/app/core/authentication.service.ts
+++ src/app/core/authentication.service.ts
@@ -1,15 +1,25 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 
 
-interface FirebaseUserResponse {
-  idToken: string,
-  email: string,
-  refreshToken: string,
-  expiresIn: string,
-  localId: string
+interface FirebaseUserSignUpResponse {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+interface FirebaseUserSignInResponse {
+  displayName: string;
+  email: string;
+  expiresIn: string;
+  idToken: string;
+  localId: string;
+  refreshToken: string;
+  registered: boolean;
 }
 
 @Injectable({
@@ -18,9 +28,31 @@ interface FirebaseUserResponse {
 export class AuthenticationService {
 
   readonly #httpClient = inject(HttpClient);
-  registerFirebaseUser(email: string, password: string): Observable<FirebaseUserResponse> {
+  registerFirebaseUser(email: string, password: string): Observable<FirebaseUserSignUpResponse> {
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseConfig.apiKey}`
-    const body = {"email":email,"password":password, "retrunSecureToke":true };
-    return this.#httpClient.post<FirebaseUserResponse>(url, body);
+    const body = {email,password, returnSecureToken:true };
+    return this.#httpClient.post<FirebaseUserSignUpResponse>(url, body);
     }
+
+  firebaseLogin(email: string, password: string): Observable <FirebaseUserSignInResponse> {
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseConfig.apiKey}`
+    const body = {email,password, returnSecureToken:true };
+    return this.#httpClient.post<FirebaseUserSignInResponse>(url, body);
+  }
+
+  writeFirebaseDatabase(email: string, userId: string, bearerToken: string): Observable<unknown> {
+    const baseUrl = `https://firestore.googleapis.com/v1/projects/${environment.firebaseConfig.projectId}/databases/(default)/documents`;
+    const firestoreCollectionId = 'users';
+    const url = `${baseUrl}/${firestoreCollectionId}?key=${environment.firebaseConfig.apiKey}&documentId=${userId}`;
+    const body = {
+      fields: {
+        email: { stringValue: email },
+      },
+    };
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${bearerToken}`
+    })
+    const options = { headers };
+    return this.#httpClient.post(url, body, options);
+  }
 }
