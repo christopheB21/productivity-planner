660b43488da334898810bf0ec208188101300844
christopheB21 <christophe.brunel21@orange.fr>
2024-12-07 19:12:45 +0100

    Live9 firestore database (#10)
    * builder(config): add production environment config
    
    * Revert "builder(config): add production environment config"
    
    This reverts commit 903f845b4d90b1319eea913416bb8825871925c2.
    
    * feat(auth): add new user firebase
    
    * fix(tests): skip authenticator service tests
    
    * build(webapp): add firestore database save document
    
    * feature(authentification service): audit fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [provideHttpClient()],
       imports: [AppComponent]
     }).compileComponents();
   });
