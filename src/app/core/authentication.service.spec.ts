0a914250b6a6175d97cccefb4b784925c0b281b2
christopheB21 <christophe.brunel21@orange.fr>
2024-12-13 16:51:10 +0100

    ci(build): setup environment secrets

--- src/app/core/authentication.service.spec.ts
+++ src/app/core/authentication.service.spec.ts
@@ -1,17 +1,17 @@
-/*import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { AuthenticationService } from './authentication.service';
 import { provideHttpClient } from '@angular/common/http';
 
 describe('AuthenticationService', () => {
-  let service: AuthenticationService;
+  let authenticationService: AuthenticationService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({providers: [provideHttpClient()]});
-    service = TestBed.inject(AuthenticationService);
+    authenticationService = TestBed.inject(AuthenticationService);
   });
 
    it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(authenticationService).toBeTruthy();
   }); 
+
 });
-*/
