ceab3418bbec28c51a4622ab2955c09e46b278f8
christopheB21 <christophe.brunel21@orange.fr>
2024-12-20 13:08:14 +0100

    bugfix: github

---
+++ src/app/core/authentication.service.spec.ts
@@ -0,0 +1,17 @@
+// import { TestBed } from '@angular/core/testing';
+// import { AuthenticationService } from './authentication.service';
+// import { provideHttpClient } from '@angular/common/http';
+
+// describe('AuthenticationService', () => {
+//   let authenticationService: AuthenticationService;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({providers: [provideHttpClient()]});
+//     authenticationService = TestBed.inject(AuthenticationService);
+//   });
+
+//    it('should be created', () => {
+//     expect(authenticationService).toBeTruthy();
+//   }); 
+
+// });
