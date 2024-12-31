c13884db08b75e7718446c2dc250e26f93127f64
christopheB21 <christophe.brunel21@orange.fr>
2024-12-31 11:14:05 +0100

    Feat(homepage): homepage implementation

---
+++ src/app/visitor/hompage/hompage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './hompage.component.html',
+  styleUrl: './hompage.component.scss'
+})
+export class HompageComponent {
+
+}
