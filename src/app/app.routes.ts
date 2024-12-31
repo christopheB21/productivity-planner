65e5103cd1caeb93c5558afffa0fb8e362ce322c
christopheB21 <christophe.brunel21@orange.fr>
2024-12-31 11:18:12 +0100

    Feat(homepage): homepage implementation (#17)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HompageComponent } from './visitor/hompage/hompage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HompageComponent, title: 'Homepage' }
+];
