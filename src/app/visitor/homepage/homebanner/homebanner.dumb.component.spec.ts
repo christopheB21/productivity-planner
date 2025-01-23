cbb0053d371c46f49d02a0b0c62f28d638a47536
christopheB21 <christophe.brunel21@orange.fr>
2025-01-23 15:37:40 +0100

    feat(homepagebanner test): all inputs tests implementation

--- src/app/visitor/homepage/homebanner/homebanner.dumb.component.spec.ts
+++ src/app/visitor/homepage/homebanner/homebanner.dumb.component.spec.ts
@@ -1,10 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomebannerDumbComponent } from './homebanner.dumb.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('HomebannerDumbComponent', () => {
   let component: HomebannerDumbComponent;
   let fixture: ComponentFixture<HomebannerDumbComponent>;
+  let debugElement: DebugElement;
+
+  let title: DebugElement;
+  let description: DebugElement;
+  let button: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,15 +21,34 @@ describe('HomebannerDumbComponent', () => {
 
     fixture = TestBed.createComponent(HomebannerDumbComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+    fixture.componentRef.setInput('title', 'expectedTitle');
+    fixture.componentRef.setInput('description', 'expectedDescription');
+    fixture.componentRef.setInput('button', 'expectedButton');
     fixture.detectChanges();
   });
+
+  beforeEach(()=> {
+    title = debugElement.query(By.css('[data-testid=banner-title]'));   
+    description = debugElement.query(By.css('[data-testid=banner-description]'));   
+    button = debugElement.query(By.css('[data-testid=banner-button]'));   
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it.todo('should disply title');
-  it.todo('should display description');
-  it.todo('should display button');
+  it('should display title', () => {
+    expect(title.nativeElement.textContent).toContain('expectedTitle');
+  });
+
+  it('should display description', () => {
+    expect(description.nativeElement.textContent).toContain('expectedDescription');
+  });
+
+  it('should display button', () => {
+    expect(button.nativeElement.textContent).toContain('expectedButton');
+  });
+  
   it.todo('should trigger event on button click');
 });
