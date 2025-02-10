ab3cff1e941b18197d3ebd1eb4bcf2cae031e4af
christopheB21 <christophe.brunel21@orange.fr>
2025-02-10 11:35:50 +0100

    bugfix(navbar test component): implements activated Routes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
+
+  const mockActivatedRoute = {
+    paramMap: of(convertToParamMap({})),
+    snapshot: {
+      paramMap: convertToParamMap({})
+    }
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent]
+      imports: [AppComponent],
+            providers: [
+              { provide: ActivatedRoute, useValue: mockActivatedRoute }
+            ]
     }).compileComponents();
   });
 
--- src/app/core/navbar/navbar.smart.component.spec.ts
+++ src/app/core/navbar/navbar.smart.component.spec.ts
@@ -1,24 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavbarSmartComponent } from './navbar.smart.component';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 describe('NavbarSmartComponent', () => {
   let component: NavbarSmartComponent;
   let fixture: ComponentFixture<NavbarSmartComponent>;
+  const mockActivatedRoute = {
+    paramMap: of(convertToParamMap({})),
+    snapshot: {
+      paramMap: convertToParamMap({})
+    }
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarSmartComponent, RouterLink]
-    })
+      imports: [NavbarSmartComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute }
+      ]
+     })
     .compileComponents();
-
     fixture = TestBed.createComponent(NavbarSmartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
+  
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have router link', () => {
+    expect(fixture.debugElement.query(By.css('a[routerLink]'))).toBeTruthy();
+  });
+
 });
