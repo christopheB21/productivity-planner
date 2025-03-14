9628f3bada19bb0f4431caaaf3b118a5e2fc5221
cbrunel <christophe.brunel@francetv.fr>
2025-03-14 15:55:17 +0100

    bugfix(ci): ajout rootDir/Src jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -125,12 +125,13 @@ const config: Config = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  // rootDir: './',
+  // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //    "<rootDir>"
-  // ],
+   roots: [
+     "<rootDir>",
+     "<rootDir>/src/"
+   ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
