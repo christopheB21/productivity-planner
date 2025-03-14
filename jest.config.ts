cfd95eed414bd92ec1f382d8dafbb9992ebc43d7
cbrunel <christophe.brunel@francetv.fr>
2025-03-14 15:05:54 +0100

    bugfix(tests): implementation of @environments path

--- jest.config.ts
+++ jest.config.ts
@@ -90,7 +90,9 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+   moduleNameMapper: {
+    "^@environments/(.*)$": "<rootDir>/src/environments/$1",
+   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -123,11 +125,11 @@ const config: Config = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  // rootDir: undefined,
+  // rootDir: './',
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
-  //   "<rootDir>"
+  //    "<rootDir>"
   // ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
