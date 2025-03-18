18ecab857cb548072566ea4069be207fb96912f5
cbrunel <christophe.brunel@francetv.fr>
2025-03-18 12:01:59 +0100

    bugfix(tests(: defazult jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -128,10 +128,9 @@ const config: Config = {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-   roots: [
-     "<rootDir>",
-     "<rootDir>/src/"
-   ],
+  // roots: [
+  //   "<rootDir>"
+  // ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
