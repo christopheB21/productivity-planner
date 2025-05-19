f108521664358456b15b0e483d963c0502672e9f
cbrunel <christophe.brunel@francetv.fr>
2025-05-19 13:00:03 +0200

    bugfix(tests): environment setup

--- src/app/core/adapter/authentication-firebase.service.ts
+++ src/app/core/adapter/authentication-firebase.service.ts
@@ -1,7 +1,7 @@
 
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { environment } from '@environments/environment';
+import { environment } from '@env/environment';
 import { catchError, map, Observable, of } from 'rxjs';
 import { AuthenticationService, EmailAlreadyExtistsError, LoginResponse, RegisterResponse } from '../port/authentication.service';
 
