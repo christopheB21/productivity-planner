0607c68cde4d385b4db024abb117ea632724e119
christopheB21 <christophe.brunel21@orange.fr>
2025-06-13 16:33:12 +0200

    Task#25 workday layout
    * wip(workday):  workday layout
    
    * fix(workday): add store provider
    
    ---------
    
    Co-authored-by: cbrunel <christophe.brunel@francetv.fr>

--- src/app/membership/workday/workday.page.component.ts
+++ src/app/membership/workday/workday.page.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { WorkdayStore } from './workday.page.store';
 
 @Component({
   imports: [],
+  providers: [WorkdayStore],
   templateUrl: './workday.page.component.html',
   styleUrl: './workday.page.component.scss'
 })
 export class WorkdayPageComponent {
-
+  readonly store = inject(WorkdayStore);
 }
