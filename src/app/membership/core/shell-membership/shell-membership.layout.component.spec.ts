d2d81da3647f51205b5ba3463b0103f59abb1293
cbrunel <christophe.brunel@francetv.fr>
2025-06-13 15:41:08 +0200

    fix(membership): fix router providing in tests

--- src/app/membership/core/shell-membership/shell-membership.layout.component.spec.ts
+++ src/app/membership/core/shell-membership/shell-membership.layout.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShellMembershipLayoutComponent } from './shell-membership.layout.component';
+import { provideRouter } from '@angular/router';
 
 describe('ShellMembershipLayoutComponent', () => {
   let component: ShellMembershipLayoutComponent;
@@ -8,7 +9,8 @@ describe('ShellMembershipLayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ShellMembershipLayoutComponent]
+      imports: [ShellMembershipLayoutComponent],
+      providers: [provideRouter([])]
     })
     .compileComponents();
 
