18e28c39be5a66a6464f800bce8ab201c826947a
cbrunel <christophe.brunel@francetv.fr>
2025-06-13 15:07:43 +0200

    wip(sidebar): side bar design

--- src/app/membership/core/shell-membership/shell-membership.layout.component.ts
+++ src/app/membership/core/shell-membership/shell-membership.layout.component.ts
@@ -1,9 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarSmartComponent } from '../sidebar/sidebar.smart.component';
 
 @Component({
-  selector: 'app-shell-membership',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarSmartComponent],
   templateUrl: './shell-membership.layout.component.html',
   styleUrl: './shell-membership.layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
