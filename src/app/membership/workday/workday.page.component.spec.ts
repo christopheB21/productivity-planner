33ef0580d8cbf72eacb4f776b1d7bacd46b9278a
cbrunel <christophe.brunel@francetv.fr>
2025-09-28 11:05:31 +0200

    feat(test): remove task test implementation

--- src/app/membership/workday/workday.page.component.spec.ts
+++ src/app/membership/workday/workday.page.component.spec.ts
@@ -1,14 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { WorkdayPageComponent } from './workday.page.component';
 import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('WorkdayPageComponent', () => {
   let component: WorkdayPageComponent;
   let fixture: ComponentFixture<WorkdayPageComponent>;
-
+  
     const getAddTaskButton = () =>
-    fixture.nativeElement.querySelector('[data-test=add-task-button]');
+    fixture.nativeElement.querySelector('[data-testid=add-task-button]');
 
     beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -25,12 +24,42 @@ describe('WorkdayPageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  describe('when there is less than 6 tasks planned for the current day', () => {
+  describe('when workday page load', () => {
+      it('shuold diplay one task', () => {
+        expect(component.store.taskCount()).toBe(1);
+      })
       it('sould display "Add task" button', () => {
         const button = getAddTaskButton();
         expect(button).toBeDefined();
       });
     });
+
+  describe('when user removes task', () => {
+    beforeEach(() => {
+      component.store.onAddTask();
+      component.store.onAddTask();
+      fixture.detectChanges();
+
+    })
+    it('should remove a task', () => {
+
+      for (let i = 0; i < component.store.taskList().length; i++) {
+        const taskItemElement = fixture.nativeElement.querySelector(`[data-testid="task${i}"]`);
+        taskItemElement.value=i.toString();
+
+        const inputEvent = new Event('input');
+        taskItemElement.dispatchEvent(inputEvent);
+
+        component.store.updateTaskTitle(i, inputEvent);
+        expect(component.store.taskList()[i].title).toBe(i.toString());
+      }
+      component.store.removeTask(1);
+      expect(component.store.taskList().length).toBe(2);
+      expect(component.store.taskList()[0].title).toBe('0');
+      expect(component.store.taskList()[1].title).toBe('2');
+
+    })
+  })
 
   describe('when there is 6 tasks planned for the current day', () => {
     beforeEach(() => {
