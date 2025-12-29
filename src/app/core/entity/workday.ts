636c53e92fc3add1bfbc2a994b2cac779c523e98
cbrunel <christophe.brunel@francetv.fr>
2025-12-29 18:25:26 +0100

    wip(workday): entity implementation

---
+++ src/app/core/entity/workday.ts
@@ -0,0 +1,45 @@
+import { TaskList } from '@app/membership/workday/task.model';
+import { Entity } from '../domain/entity.class';
+
+interface WorkdayProps {
+      // date: string;
+      taskList: TaskList;
+      progress: number;
+      mode: 'edit' | 'execution';
+}
+
+export class Workday extends Entity<WorkdayProps> {
+    static create(props: WorkdayProps, date: string): Workday {
+        //Validation
+        return new Workday(props, date);
+    }
+
+    static createEmpty(): Workday {
+        const now = new Date().toLocaleString();
+        const emptyPros: WorkdayProps = {
+            taskList: [],
+            progress: 0,
+            mode: 'edit',
+        };
+        return Workday.create(emptyPros, now);
+    }
+
+}
+
+
+export class Interval extends ValueObject<IntervalProps> {
+  static create(value: IntervalName): Interval {
+    if (!this.isValid(value)) {
+      throw new Error('A valid interval name is required.');
+    }
+    return new Interval({ value });
+  }
+
+  private isValid(value: unknown): value is IntervalName {
+    return IntervalList.includes(value);
+  }
+
+  get value(): IntervalName {
+    return this.props.value;
+  }
+}
